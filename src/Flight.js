114f2e0a65ad2d90b02ba04ec55dc1c4d34ef352
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-06 12:31:43 -0500

    add buttons to flight card

--- src/Flight.js
+++ src/Flight.js
@@ -5,6 +5,11 @@ function Flight({date, plane, id, description}) {
         <div className='flight-card'>
             <h3 className='flight-card-date'>{date}</h3>
             <p className='flight-card-description'>{description}</p>
+            <span className='flight-card-buttons'>
+                <button>Add</button>
+                <button>Edit</button>
+                <button>Delete</button>
+            </span>
         </div>
     )
 }
