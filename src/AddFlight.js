0d6201bfb2c3dbd3e149454186bed732995ac342
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-06 13:06:46 -0500

    create AddFlight component

--- src/AddFlight.js
+++ src/AddFlight.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import {FlightContext} from './FlightContext'
 
 const AddFlight = () => {
 
     const [date, setDate] = useState('')
     const [description, setDescription] = useState('')
+    const [flights, setFlights] = useContext(FlightContext)
 
     const updateName = e => {
         setDescription(e.target.value)
@@ -11,10 +13,16 @@ const AddFlight = () => {
     const updateDate = e => {
         setDate(e.target.value)
     }
+    const addFlight = e => {
+        e.preventDefault()
+        setFlights(flights => (
+            [...flights, {date: date, description: description} ]
+        ))
+    }
     return (
-       <form>
-           <input type='date' name='date' value={date} onChange={updateDate}/>
-           <input type='text' name='description' value={description} onChange={updateName}/>
+       <form onSubmit={addFlight} className='add-flight'>
+           <input type='date' className='date' name='date' value={date} onChange={updateDate}/>
+           <input type='text' className='description' name='description' value={description} onChange={updateName}/>
            <button>Submit</button>
        </form>
     )
