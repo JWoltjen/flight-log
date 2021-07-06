ab5f1ab0022f7cbc4e2ca7caf5855e7c160cde56
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-06 13:32:15 -0500

    add delete flight function

--- src/Flight.js
+++ src/Flight.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
+import { FlightContext } from './FlightContext'
 
 function Flight({date, plane, id, description}) {
+    const [flights, setFlights] = useContext(FlightContext)
+
+    const deleteFlight = () => {
+        console.log('you clicked me')
+        setFlights(flights.filter((el) => el.id !== flights.id));
+  }
+
     return (
         <div className='flight-card'>
             <h3 className='flight-card-date'>{date}</h3>
@@ -8,7 +16,7 @@ function Flight({date, plane, id, description}) {
             <span className='flight-card-buttons'>
                 <button>Add</button>
                 <button>Edit</button>
-                <button>Delete</button>
+                <button onClick={deleteFlight}>Delete</button>
             </span>
         </div>
     )
--- src/FlightLog.js
+++ src/FlightLog.js
@@ -1,10 +1,22 @@
-import React, {useState, useContext} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import Flight from './Flight'
 import {FlightContext} from './FlightContext'
 
 function FlightLog() {
     const [flights, setFlights] = useContext(FlightContext)
 
+    useEffect(() => {
+         return (
+        <div className='flight-log-container'>
+            <div className='flight-log'>
+            {flights.map(flight => (
+                <Flight date={flight.date} plane={flight.plane} id={flight.id} description={flight.description} />
+            ))}
+            </div>
+        </div>
+    )
+    }, [flights])
+    
     return (
         <div className='flight-log-container'>
             <div className='flight-log'>
