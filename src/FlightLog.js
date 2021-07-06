bbb990aed0ba870d7b74d30d2566d17b51a7d2eb
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-06 15:49:44 -0500

    style flight-log

--- src/FlightLog.js
+++ src/FlightLog.js
@@ -7,24 +7,20 @@ function FlightLog() {
 
     useEffect(() => {
          return (
-        <div className='flight-log-container'>
             <div className='flight-log'>
             {flights.map(flight => (
                 <Flight date={flight.date} plane={flight.plane} id={flight.id} description={flight.description} />
             ))}
             </div>
-        </div>
     )
     }, [flights])
-    
+
     return (
-        <div className='flight-log-container'>
             <div className='flight-log'>
             {flights.map(flight => (
                 <Flight date={flight.date} plane={flight.plane} id={flight.id} description={flight.description} />
             ))}
             </div>
-        </div>
     )
 }
 
