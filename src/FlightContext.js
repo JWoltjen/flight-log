07217c5abbbbd81014fdd3b499c676df9a2e861c
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-06 21:20:28 -0500

    add content to original log

--- src/FlightContext.js
+++ src/FlightContext.js
@@ -7,16 +7,16 @@ export const FlightProvider = props => {
         {
             date: 'July 4, 2021',
             id: 13214, 
-            description: 'I flew a plane from Gudata to the Bullseye and back.',
+            description: 'Took off from Gudata at 1:30 and flew to the CAP point at 345. Encountered a single MiG in the contrails and engaged at 35,000ft. Fought until deck and MiG crashed into a mountain. Low of fuel, I returned to Sochi airfield.',
             kills: 1, 
             wingmen: 0, 
         }, 
         {
             date: 'July 3, 2021', 
             id: 13213, 
-            description: 'I flew a Sabre from Gudata to Sochi and back again.', 
-            kills: 0, 
-            wingmen: 3, 
+            description: 'Flew in a two-ship with BabyFace. Took off from Gudata without external tanks. 1 MiG-15 had been spotted north of the Bullseye so we flew heading 345 towards the Bullseye. We encountered a singleton MiG-15bis whereupon we engaged. I followed the MiG15 to the deck in a series of tight, one circles, with the MiG slowly gaining advantage because of its superior sustained turn rate. Whereupon, I instructed my no. 2 to stay on the MiG, and I exited the circle fight to regain energy. 30 seconds later, I re-entered the circle, slotted behind the MiG, and shot it down. Babyface and I then returned to Gudata. I claim 1 MiG destroyed.', 
+            kills: 1, 
+            wingmen: 1, 
         }
     ])
 
